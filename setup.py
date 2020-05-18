17d3b7a163cea236a7a12b4550fa0cb303e82a0f
Luc zeng <luc.zeng@hotmail.fr>
2020-05-18 17:05:37 +0200

    mlflow setup

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     name="motion_blur",
     use_scm_version=True,
     install_requires=["numpy", "matplotlib", "pyyaml", "torch", "torchvision"],
-    extras_require={"TEST_SUITE": ["pytest", "black==19.10b0", "pylint", "flake8"]},
+    extras_require={"TEST_SUITE": ["pytest", "black==19.10b0", "pylint", "flake8"],"DEVELOP": ["mlflow"]},
     packages=find_packages(),
 )
